dd2409a0df009a04c78700ee5f571d157d555853
vladimir.grinenko <S1vjycnhbr>
2024-01-04 18:20:22 +0300

    feat: add isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -147,8 +147,14 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  if (arr.length === 0) {
+    return true;
+  }
+
+  const firstStrLen = arr[0].length;
+
+  return arr.every((str) => str.length === firstStrLen);
 }
 
 /**
